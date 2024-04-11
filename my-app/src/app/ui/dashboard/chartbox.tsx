aad255f5235a9611164f6e361ecdf94cb9c0e974
Antpapa99 <anthony.oram@outlook.com>
2024-04-11 17:06:53 +0200

    fetch commune now works in a simple data foramt

--- my-app/src/app/ui/dashboard/chartbox.tsx
+++ my-app/src/app/ui/dashboard/chartbox.tsx
@@ -3,8 +3,12 @@ export default function ChartBox() {
     return (
       <>
         <section className="flex my-10 px4 gap-20">
-        <div className="w-[400px] h-[300px] bg-gray-700 rounded"></div>
-        <div className="w-[400px] h-[300px] bg-gray-700 rounded"></div>
+        <div className="w-[400px] h-[300px] bg-gray-700 rounded">
+            <p> Hello world</p>
+        </div>
+        <div className="w-[400px] h-[300px] bg-gray-700 rounded">
+        <p> Hello world</p>
+        </div>
         </section>
         <section className="flex my-10 px4 gap-3">
         <p> SLIDER HERE</p>
