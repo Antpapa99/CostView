77bdf274267da377b26a6141b650bbd41c6d7f77
Antpapa99 <anthony.oram@outlook.com>
2024-04-11 15:55:30 +0200

    added boxes

--- my-app/src/app/dashboard/commune/page.tsx
+++ my-app/src/app/dashboard/commune/page.tsx
@@ -1,4 +1,16 @@
+import styles from '@/app/ui/dashboard.module.css';
+import ChartBox from '@/app/ui/dashboard/chartbox';
 
-export default function Page () {
-    return <p>Commune</p>
+export default function Page({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
+      <div className="w-full flex-none md:w-150"> {/* Size of boxes */}
+        <ChartBox />
+      </div>
+      <div className="grow p-6 md:overflow-y-auto md:p-12">{children}</div>
+    </div>
+  );
 }
+
+
+
