487e4d0f7efa93dfcfa7fe1b28e2a7d4a8e60190
Antpapa99 <anthony.oram@outlook.com>
2024-04-12 13:50:28 +0200

    added new file

---
+++ my-app/src/app/lib/placeholder-data.ts
@@ -0,0 +1,72 @@
+const communeData = {
+        commune_name: "Bjurholms-kommun",
+        id: 2,
+        technologies: [
+            {
+                Antal_installationer: 10,
+                Arlig_besparing_per_installation_SEK: 10000,
+                Kostnad_per_installation: 4000,
+                Mojliga_installationer: 300,
+                id: 8,
+                slug: "digitala-las",
+                tech_name: "Digitala lås"
+            },
+            {
+                Antal_installationer: 10,
+                Arlig_besparing_per_installation_SEK: 25000,
+                Kostnad_per_installation: 3000,
+                Mojliga_installationer: 300,
+                id: 9,
+                slug: "digital-tillsyn-dag",
+                tech_name: "Digital tillsyn (dag)"
+            },
+            {
+                Antal_installationer: 50,
+                Arlig_besparing_per_installation_SEK: 15000,
+                Kostnad_per_installation: 5000,
+                Mojliga_installationer: 400,
+                id: 10,
+                slug: "digital-tillsyn-natt",
+                tech_name: "Digital tillsyn (natt)"
+            },
+            {
+                Antal_installationer: 25,
+                Arlig_besparing_per_installation_SEK: 20000,
+                Kostnad_per_installation: 10000,
+                Mojliga_installationer: 50,
+                id: 11,
+                slug: "lakemedelsrobot",
+                tech_name: "Läkemedelsrobot"
+            },
+            {
+                Antal_installationer: -1,
+                Arlig_besparing_per_installation_SEK: -1,
+                Kostnad_per_installation: -1,
+                Mojliga_installationer: -1,
+                id: 12,
+                slug: "digitalt-larm-gps",
+                tech_name: "Digitalt larm (GPS)"
+            },
+            {
+                Antal_installationer: -1,
+                Arlig_besparing_per_installation_SEK: -1,
+                Kostnad_per_installation: -1,
+                Mojliga_installationer: -1,
+                id: 13,
+                slug: "digitalt-larm-trygghet",
+                tech_name: "Digitalt larm (trygghet)"
+            },
+            {
+                Antal_installationer: -1,
+                Arlig_besparing_per_installation_SEK: -1,
+                Kostnad_per_installation: -1,
+                Mojliga_installationer: -1,
+                id: 14,
+                slug: "fallprevention",
+                tech_name: "Fallprevention"
+            }
+        ]
+    };
+
+    
+export default communeData;
