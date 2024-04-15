180a025cfe641c9624451a45da7377966bf49d64
Antpapa99 <anthony.oram@outlook.com>
2024-04-16 01:38:14 +0200

    progress on redesigning the chartbox layout

--- my-app/src/app/ui/dashboard/commune/chartbox.tsx
+++ my-app/src/app/ui/dashboard/commune/chartbox.tsx
@@ -15,32 +15,49 @@ export default function ChartBox() {
   
   return (
     <>
-    <section className="justify-center">
-      <div className="">
-        <CommuneDropdownItem onCommuneChange={handleCommuneChange}/>
+    <section className="flex m-2 gap-2">
+      <div className="flex-1 px-1 bg-red-700 ">
+      </div>
+      <div className="flex-1 px-1 bg-red-700 ">
+      </div>
+      <div className="flex-1 px-1 bg-red-700 ">
+      </div>
+      <div className="flex-1 px-2 justify-center w-16 bg-gray-700">
+            <div className="flex-1 px-2 justify-center">
+              <CommuneDropdownItem onCommuneChange={handleCommuneChange}/></div>
+          </div>
+          <div className="flex-1 px-1 bg-red-700 ">
+      </div>
+      <div className="flex-1 px-1 bg-red-700 ">
+      </div>
+      <div className="flex-1 px-1 bg-red-700 ">
       </div>
     </section>
       <section>
         <div className="flex m-4 gap-2">
+          <div className="flex-1 px-2 justify-center w-16 bg-gray-800 max-h-200px">
+            <div className="">
+            </div>
+          </div>
           <div className="flex-1 px-2 justify-center w-16 bg-gray-700 shadow rounded max-h-200px">
             <div className="">
             <CommuneAvgAltCostChart communeName={selectedCommune}/>
             </div>
           </div>
-          <div className="flex-1 px-2 justify-center w-16  bg-gray-700 shadow rounded max-h-300px">
+          <div className="flex-1 px-2 justify-center w-16 bg-gray-800  max-h-200px">
             <div className="">
             </div>
           </div>
         </div>
       </section>
       <section className="flex my-4 px-4 gap-3">
-        <div className="w-1/2 h-[300px] bg-gray-700 rounded">
+        <div className="w-1/2 h-[550px] bg-gray-700 rounded">
         <PenValueChart communeName={selectedCommune}/>
         </div>
-        <div className="w-1/2 h-[300px] bg-gray-700 rounded">
+        <div className="w-1/2 h-[550px] bg-gray-700 rounded">
         <TotalCostChart communeName={selectedCommune}/>
         </div>
-        <div className="w-1/2 h-[300px] bg-gray-700 rounded">
+        <div className="w-1/2 h-[550px] bg-gray-700 rounded">
         <AltCostChart communeName={selectedCommune}/>
         </div>
       </section>
