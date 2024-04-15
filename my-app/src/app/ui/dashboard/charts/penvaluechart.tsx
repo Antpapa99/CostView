888d490d224386aa4139d52d8693969125e97d94
Dyrker <elmer.blixt@gmail.com>
2024-04-15 13:28:14 +0200

    penetrationschart fixat
    ChartJS.register kan behöva fixas

---
+++ my-app/src/app/ui/dashboard/charts/penvaluechart.tsx
@@ -0,0 +1,88 @@
+'use client'
+import { useEffect, useState } from 'react';
+import { Bar } from 'react-chartjs-2';
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend,
+} from 'chart.js/auto';
+import { fetchSpecificCommune } from '@/app/lib/data';
+import { calculateCostSpecificCommune, getSpecficCommuneCost } from '@/app/lib/utils';
+import ChartDataLabels from 'chartjs-plugin-datalabels';
+import { DataLabel } from '@syncfusion/ej2-react-charts';
+
+const barColors = ["#1f77b4", "#ff7f0e", "#2ca02c"]
+
+ChartJS.register (
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    ChartDataLabels,
+    Title,
+    Tooltip,
+    Legend,
+)
+
+export const options = {
+    indexAxis: 'y' as const,
+    elements: {
+      bar: {
+      },
+    },
+    responsive: true,
+    plugins: {
+        datalabels: {
+           display: true,
+           color: 'white',
+           formatter: (value) => { // Custom formatter function to add "%" and remove decimals
+            return value.toFixed(0) + '%';
+        }
+        }
+     }
+  };
+
+const CommuneName = 'Bjurholms-kommun'
+
+export default function PenValueChart() {
+    // State används för att hantera data som ändras över tid i en react komponent vilket är det över
+    // Genom att ge penetrationCost, setPenetrationCost tuples en useState så kan UI uppdatera
+    const [communeCost, setCommuneCost] = useState<any[]>([]); 
+  
+    /* UseEffect hook som du ser här nere kan användas för att utföra data  fetching eller ändringar i DOM, 
+    useEffecten hooken tar en funktion som argument som kommer att aktiveras efter rendering i DOM */
+    useEffect(() => {
+      const fetchCommuneCost = async () => { /* Async är där så att webbsidan inte aktivera funktionen innan fetchingen är färdig */
+        const penCost = await getSpecficCommuneCost(CommuneName); /* Await vänter när den första funktionen är färdig med sitt syfte */
+        setCommuneCost(penCost); /*denna variablen unppdatera sidan med det nya */
+      };
+  
+      fetchCommuneCost(); /* säger till att funktionen körs på DOM, alltså sidan uppdateras */
+    }, []);
+
+    const backgroundColor = ['rgba(186, 0, 0, 0.7',
+    'rgba(184, 186, 0, 0.7)', 'rgba(0, 186, 176, 0.7)', 'rgba(80, 0, 186, 0.7)', 'rgba(33, 186, 0, 0.7)']
+
+    const chartData = {
+        labels: communeCost.map(data => data.techName), // Tänk map som en foreach
+        datasets: [
+            {
+                label: "Penetrationsgrad",
+                data: communeCost.map(data => data.penCost),
+                backgroundColor: backgroundColor,
+                borderWidth: 1
+            }
+        ]
+    }
+
+    return (
+        <div className = "PenValueChart">
+            <Bar 
+            data={chartData}
+            options = {options}
+            /> </div>
+    )
+};
