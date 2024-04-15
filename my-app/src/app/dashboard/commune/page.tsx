70c649a9c4b5da8c26dedb0e8f99557068d0cd18
Antpapa99 <anthony.oram@outlook.com>
2024-04-15 22:01:07 +0200

    remove react node

--- my-app/src/app/dashboard/commune/page.tsx
+++ my-app/src/app/dashboard/commune/page.tsx
@@ -2,14 +2,17 @@
 import ChartBox from '@/app/ui/dashboard/commune/chartbox';
 import React from 'react';
 
+type DeployComponentProps = {
+  children?: React.ReactNode;
+}
 
-export default function Page({ children }: { children: React.ReactNode }) {
+export default function Page({props: DeployComponentProps }) {
   return (
     <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
       <div className="w-full flex-none md:w-150"> {/* Size of boxes */}
       <ChartBox />
       </div>
-      <div className="grow p-6 md:overflow-y-auto md:p-12">{children}</div>
+      <div className="grow p-6 md:overflow-y-auto md:p-12">{DeployComponentProps}</div>
     </div>
   );
 }
