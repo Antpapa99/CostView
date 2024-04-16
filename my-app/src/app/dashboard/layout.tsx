41c1ae10187f7930f3832ee57b5f64658148328f
Antpapa99 <anthony.oram@outlook.com>
2024-04-16 15:39:26 +0200

    Dashboard should look better now

--- my-app/src/app/dashboard/layout.tsx
+++ my-app/src/app/dashboard/layout.tsx
@@ -7,7 +7,7 @@ interface PageProps {
 export default function Layout({ children }: PageProps) {
   return (
     <div className="bg-gradient-to-r from-purple-100 to-gray-400 flex h-screen flex-col md:flex-row md:overflow-hidden">
-      <div className="bg-gradient-to-r from-purple-100 to-gray-400 w-full flex-none md:w-64">
+      <div className="bg-blue-100 w-full flex-none md:w-64 shadow rounded">
         <SideNav  />
       </div>
       <div className="grow p-6 md:overflow-y-auto md:p-12">{children}</div>
