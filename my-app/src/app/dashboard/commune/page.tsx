8ad9e98e0b1f4592a2c0d5d49d5bbe74bf2e2bd1
Antpapa99 <anthony.oram@outlook.com>
2024-04-19 23:04:13 +0200

    Moved the state management of the commune dashboard to the pages.tsx

--- my-app/src/app/dashboard/commune/page.tsx
+++ my-app/src/app/dashboard/commune/page.tsx
@@ -5,17 +5,23 @@ import ChartBox from '@/app/ui/dashboard/commune/communeoverlay';
 import { useSearchParams } from 'next/navigation';
 import { useState, useEffect } from 'react';
 import { useRouter } from 'next/navigation'
+import CommuneDropdownItem from '@/app/ui/dashboard/commune/communedropdown';
 
 interface PageProps {
   children: React.ReactNode;
 }
 
 export default function Page({ children }: PageProps) {
+  const [selectedCommune, setSelectedCommune] = useState();
+  const handleCommuneChange = (kommun: any) => {
+    setSelectedCommune(kommun)};
+    
   return (
     
-    <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
+    <div className="flex h-screen flex-col md:flex-col md:overflow-hidden">
+      <CommuneDropdownItem onCommuneChange={handleCommuneChange} />
       <div className="w-full flex-none md:w-150"> {/* Size of boxes */}
-      <ChartBox />
+      <ChartBox selectedCommune = {selectedCommune} />
       </div>
       <div className="grow p-6 md:overflow-y-auto md:p-12">{children}</div>
     </div>
--- my-app/src/app/ui/dashboard/commune/communeoverlay.tsx
+++ my-app/src/app/ui/dashboard/commune/communeoverlay.tsx
@@ -9,11 +9,7 @@ import SavingsPotetialChart from "../charts/commune/communesavingspotetialchart"
 import { useRouter } from "next/router";
 import { useSearchParams } from "next/navigation";
 
-export default function ChartBox() {
-  const [selectedCommune, setSelectedCommune] = useState();
-  const handleCommuneChange = (kommun: any) => {
-    setSelectedCommune(kommun)};
-  
+export default function ChartBox({selectedCommune}: any ) {
   
   return (
     <>
@@ -21,7 +17,6 @@ export default function ChartBox() {
       <div className="flex-1 px-1 bg-red-700"></div>
       <div className="flex-1 px-2 justify-center md:w-auto bg-blue-100">
         <div className="flex-1 h-full px-2 justify-center">
-          <CommuneDropdownItem onCommuneChange={handleCommuneChange} />
         </div>
       </div>
       <div className="flex-1 px-1 bg-red-700"></div>
