9e4866211a20b719689fc76de0505f46c65b917d
Antpapa99 <anthony.oram@outlook.com>
2024-04-20 23:01:45 +0200

    working more on the national

--- my-app/src/app/ui/dashboard/national/nationaloverlay.tsx
+++ my-app/src/app/ui/dashboard/national/nationaloverlay.tsx
@@ -7,10 +7,15 @@ import NationalAvgPenGradeChart from "../charts/national/nationalavgpenchart";
 export default function NationalOverlay() {
   return (
     <>
-      <section className="flex flex-row my-1 px1 gap-5">    
+      <section className="flex flex-row my-1 px1 gap-5">
+      <div className="flex flex-col my-1 px1 gap-5">  
           <div className="flex h-96 w-full bg-blue-100 rounded gap-3">
           <NationalAvgPenGradeChart />
           </div>
+          <div className="flex h-auto w-full bg-blue-100 rounded gap-3">
+          <NationalAvgAltCostChart />
+          </div>
+      </div>
           <div className="flex h-96 w-full bg-blue-100 rounded gap-3">
           <ScatterPlot  />        
           </div>
