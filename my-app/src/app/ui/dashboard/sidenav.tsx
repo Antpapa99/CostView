6b77d6fb20f0688f33dcce12bb9cf5dbf73cff68
Antpapa99 <anthony.oram@outlook.com>
2024-04-21 22:08:35 +0200

    added logo

--- my-app/src/app/ui/dashboard/sidenav.tsx
+++ my-app/src/app/ui/dashboard/sidenav.tsx
@@ -1,15 +1,25 @@
 import { Button } from '@nextui-org/react';
 import Link from 'next/link';
 import NavLinks from '@/app/ui/dashboard/nav-links';
+import Image from 'next/image'
+import logo from '/public/costviewlogo.png'
 export default function SideNav() {
   return (
     <div className="bg-black-600 flex h-full flex-col px-3 py-4 md:px-2">
+      
+      
       <Link
         className="mb-2 flex h-20 items-end justify-start rounded-md bg-blue-500 p-4 md:h-40"
         href="./alternativkostnad"
       >
         <div className="w-32 bg-blue-100 md:w-40">
         </div>
+        <Image
+      src={logo}
+      width={900}
+      height={900}
+      alt="Please load"
+    />
       </Link>
       <div className="flex grow flex-row bg-blue-200 justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
         <NavLinks />
