84e400591f9851e0be8eec7e3980d8063f487dbf
Antpapa99 <anthony.oram@outlook.com>
2024-04-25 13:14:45 +0200

    navbar

--- my-app/src/app/ui/dashboard/sidenav.tsx
+++ my-app/src/app/ui/dashboard/sidenav.tsx
@@ -9,10 +9,10 @@ export default function SideNav() {
       
       
       <Link
-        className="mb-2 flex h-20 items-end justify-start rounded-md bg-blue-500 p-4 md:h-40"
+        className="mb-2 flex h-20 items-end justify-start rounded-md bg-gradient-to-r from-violet-600 to-indigo-600 p-4 md:h-40"
         href="./alternativkostnad"
       >
-        <div className="w-32 bg-blue-100 md:w-40">
+        <div className="w-32 bg-gradient-to-r from-violet-600 to-indigo-600 md:w-40">
         </div>
         <Image
       src={logo}
@@ -21,9 +21,9 @@ export default function SideNav() {
       alt="Please load"
     />
       </Link>
-      <div className="flex grow flex-row bg-blue-200 justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
+      <div className="flex grow flex-row bg-gradient-to-r from-violet-600 to-indigo-600 justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
         <NavLinks />
-        <div className="hidden h-auto w-full grow rounded-md bg-blue-200 md:block"></div>
+        <div className="hidden h-auto w-full grow rounded-md bg-gradient-to-r from-violet-600 to-indigo-600 md:block"></div>
       </div>
     </div>
   );
