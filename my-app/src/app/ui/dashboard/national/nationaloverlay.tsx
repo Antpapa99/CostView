3df88e84436d9c58a16b732d997ccf8c8ec10f66
Dyrker <elmer.blixt@gmail.com>
2024-04-26 15:15:28 +0200

    Update nationaloverlay.tsx

--- my-app/src/app/ui/dashboard/national/nationaloverlay.tsx
+++ my-app/src/app/ui/dashboard/national/nationaloverlay.tsx
@@ -11,13 +11,16 @@ export default function NationalOverlay() {
       <section className="flex h-full gap-3">
         <section  className="w-1/2 h-full flex flex-col gap-3"> 
 
-            <div className="flex-auto w-full px-1 h-1/2 bg-gray-800 ">             
-                      <NationalAvgPenGradeChart />           
-            </div>
 
             <div className="flex-auto w-full h-1/2 rounded bg-gray-800 ">
                     <NationalAltCostChart/>
             </div>
+            
+            <div className="flex-auto w-full px-1 h-1/2 bg-gray-800 ">             
+                      <NationalAvgPenGradeChart />           
+            </div>
+
+
         </section>
 
           <div className="flex-auto h-full w-1/2 rounded bg-gray-800">         
