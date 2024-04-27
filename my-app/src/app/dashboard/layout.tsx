0918b40a0ae23db1f3a7ed2a88dbd2372f03fb6f
Antpapa99 <anthony.oram@outlook.com>
2024-04-27 13:58:30 +0200

    more potentiel fixes

--- my-app/src/app/dashboard/layout.tsx
+++ my-app/src/app/dashboard/layout.tsx
@@ -11,7 +11,7 @@ export default function Layout({ children }: PageProps) {
       <div className="bg-gradient-to-r from-violet-600 to-indigo-600 w-full flex-relative md:w-64 shadow rounded ">
         <SideNav  />
       </div>
-      <div className="flex-grow p-1 md:overflow-y-auto md:p-1">{children}</div>
+      <div className="flex-grow p-1 overflow-scroll md:overflow-hidden md:p-1">{children}</div>
     </div>
   );
 }
