c1f4a85a1972042d067242e9757ce4df185d830e
Antpapa99 <anthony.oram@outlook.com>
2024-04-28 14:08:08 +0200

    new changes

--- my-app/src/app/dashboard/layout.tsx
+++ my-app/src/app/dashboard/layout.tsx
@@ -8,7 +8,7 @@ interface PageProps {
 export default function Layout({ children }: PageProps) {
   return (
     <div className="flex h-full w-full  bg-gray-900 overflow-auto flex-row md:flex-row md:overflow-auto">
-      <div className="bg-gradient-to-r from-violet-600 to-indigo-600 w-full flex-relative md:w-64 shadow rounded ">
+      <div className="bg-gradient-to-r from-violet-600 to-indigo-600 w-64 flex-none md:w-64 shadow rounded ">
         <SideNav  />
       </div>
       <div className="p-4 md:overflow-hidden md:p-1">{children}</div>
